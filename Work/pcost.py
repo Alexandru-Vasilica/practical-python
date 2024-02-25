b7e48bdda7ed80dacb4faf475213f5ec1fb7c804
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2024-02-25 19:45:21 +0200

    Day 2

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,32 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    total = 0
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        header = next(rows)
+        for line, row in enumerate(rows):
+            try:
+                record = dict(zip(header, row))
+                total += int(record['shares']) * float(record['price'])
+            except ValueError:
+                print(f'Row {line}: Could not convert {row}')
+
+    return total
+
+
+if len(sys.argv) >= 2:
+    filename = sys.argv[1]
+else:
+    filename = input("Provide a filename:")
+try:
+    cost = portfolio_cost(filename)
+    print(cost)
+except FileNotFoundError:
+    print("Invalid filename")
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,75 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+portfolio_file = "Data/portfolio.csv"
+prices_file = "Data/prices.csv"
+
+
+def read_portfolio_tuple(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            entry = row[0], int(row[1]), float(row[2])
+            portfolio.append(entry)
+    return portfolio
+
+
+def read_portfolio_dict(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            entry = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(entry)
+    return portfolio
+
+
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row):
+                prices[row[0]] = float(row[1])
+    return prices
+
+
+def calculate_portfolio_value(portfolio_filename, prices_filename):
+    total = 0
+    portfolio = read_portfolio_tuple(portfolio_filename)
+    prices = read_prices(prices_filename)
+    for name, shares, price in portfolio:
+        total += shares * (prices[name] - price)
+    return total
+
+
+def make_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio_tuple(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = []
+    for name, shares, price in portfolio:
+        change = shares * (prices[name] - price)
+        entry = name, shares, price, change
+        report.append(entry)
+    return report
+
+
+def print_report(report):
+    headers = ['Name', 'Shares', 'Price', 'Change']
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+    print(f'{"":->10} {"":->10} {"":->10} {"":->10}')
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {f'${price:.2f}':>9s} {change:>10.2f}')
+
+
+report = make_report(portfolio_file, prices_file)
+print_report(report)
